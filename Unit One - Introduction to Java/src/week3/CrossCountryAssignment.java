f68077307d8751803cef6fa75794dece7dffdbee
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-02-10 10:52:48 -0500

    Added Stub for CrossCountryAssignment

---
+++ Unit One - Introduction to Java/src/week3/CrossCountryAssignment.java
@@ -0,0 +1,43 @@
+package week3;
+
+import java.util.Scanner;
+
+public class CrossCountryAssignment {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    processRunner(in);  // runner 1
+    processRunner(in);  // runner 2
+    processRunner(in);  // runner 3
+
+    in.close();
+  }
+
+  private static void processRunner(Scanner in) {
+    System.out.print("Please enter runner's first Name");
+    String fName = in.nextLine();
+
+    System.out.print("Please enter runner's Last Name");
+    String lName = in.nextLine();
+
+
+  }
+
+  private static String subtractTimes(String startTime, String finishTime){
+    return null;
+  }
+
+  /**
+   * Converts a time into a doubld (seconds) "5:34.221" => 334.221
+   * 
+   * @param timeString time in the format "mm:ss.sss"
+   * @return converts time into seconds
+   */
+  private static double convertToSeconds(String timeString) {
+    int colon = timeString.indexOf(":");
+    int minutesAsSeconds = Integer.parseInt(timeString.substring(0,colon)) * 60;
+    double seconds = Double.parseDouble(timeString.substring(colon+1));
+
+    return minutesAsSeconds + seconds;
+  }
+}
