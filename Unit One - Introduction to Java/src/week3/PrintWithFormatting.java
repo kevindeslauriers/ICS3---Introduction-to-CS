78aae9bd13d7d70784f84898dc9f2880317839b9
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-02-13 17:02:49 -0500

    Formatting numbers using String.format()

--- Unit One - Introduction to Java/src/week3/PrintWithFormatting.java
+++ Unit One - Introduction to Java/src/week3/PrintWithFormatting.java
@@ -1,16 +1,30 @@
 package week3;
 
+/**
+ * We can format our numbers so that they take a specific width of the screen.
+ * We can also specify the number of decimal places we want.
+ * Lastly, we can pad numbers with zeros (on the left)
+ * 
+ * TTis is all done using String.format() which takes a string containing placeholders (%d and %f)
+ * Watch the video in Teams for a full description on how it works.
+ */
 public class PrintWithFormatting {
   public static void main(String[] args) {
-    double sec = 1.2343341;
-    int min = 6;
+    // 11:25.393
 
-    //  6:01.234
+    // 5:06.270
+    // 5:6.27
+    // 5:6.2900000000003
 
-    System.out.printf("%d:%09.3f", min, sec);
-    String formattedString = String.format("%d:%09.3f", min, sec);
+    int minutes = 5;
+    double seconds = 6.27;
 
-    System.out.println(formattedString);  //  6:01.234
+    String timeFormatted = String.format("%d:%06.3f", minutes,  seconds); 
+    System.out.println(timeFormatted);  // 5:06.270
+
+
+    double money = 6.2;
+    System.out.println(String.format("$%4.2f", money)); 
 
   }
 }
