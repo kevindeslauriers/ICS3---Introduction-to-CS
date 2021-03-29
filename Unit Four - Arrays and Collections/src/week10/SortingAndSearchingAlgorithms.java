be599c85d5b0509b18a677fb593ac8fd689cee9c
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-03-29 09:56:30 -0400

    Added Merge Method

--- Unit Four - Arrays and Collections/src/week10/SortingAndSearchingAlgorithms.java
+++ Unit Four - Arrays and Collections/src/week10/SortingAndSearchingAlgorithms.java
@@ -5,11 +5,11 @@ public class SortingAndSearchingAlgorithms {
     int nums[] = { 10, 2, 18, -4, 12, 33, 9 };
 
     selectionSort(nums);
-    display(nums);
+    // display(nums);
 
     int nums1[] = { 12, 21, -18, -4, 2, 13, 9 };
     insertionSort(nums1);
-    display(nums1);
+    // display(nums1);
 
     int arr[] = merge(nums, nums1);
     display(arr);
@@ -22,7 +22,32 @@ public class SortingAndSearchingAlgorithms {
    * @return a sorted array with all of the elements from nums and nums2
    */
   private static int[] merge(int[] nums, int[] nums1) {
+    int arr[] = new int[nums.length + nums1.length];
+    int i = 0;
+    int j = 0;
 
+    for (int k = 0; k < arr.length; k++) {
+      // check if we have NOT reached the end of both arrays
+      if (i < nums.length && j < nums1.length) {
+        if (nums[i] < nums[j]) {
+          arr[k] = nums[i];
+          i++;
+        } else {
+          arr[k] = nums1[j];
+          j++;
+        }
+        // checks if we have stuff left in the 1st array
+      } else if (i < nums.length) {
+        arr[k] = nums[i];
+        i++;
+        // we have stuff left only in the 2nd array
+      } else {
+        arr[k] = nums1[j];
+        j++;
+      }
+    }
+
+    return arr;
   }
 
   private static void insertionSort(int[] arr) {
